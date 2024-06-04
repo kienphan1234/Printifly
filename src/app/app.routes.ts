a6c159dba17de67ee47dd6ae8c33c4d9ecdb2f9a
kiên <91230993+kienphan1234@users.noreply.github.com>
2024-06-04 14:09:24 +0700

    up code

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ProductComponent } from './pages/product/product.component';
+
+export const routes: Routes = [
+    {
+        path: 'product',
+        component: ProductComponent
+    }
+];

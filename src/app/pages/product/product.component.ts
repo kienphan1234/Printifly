b81c3512bb3db7899d14494ad9aa70060240e0b5
kiên <91230993+kienphan1234@users.noreply.github.com>
2024-06-14 16:16:16 +0700

    update

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+  firstImage = 'https://printify.com/cdn-cgi/image/width=320,quality=100,format=avif/https://images.printify.com/api/catalog/63523020339ea2ad9709f692.jpg';
+  secondImage = 'https://printify.com/cdn-cgi/image/width=520,quality=100,format=avif/https://images.printify.com/api/catalog/62d66a1a35154d9068013c66';
+  currentImage = this.firstImage;
 
+  changeImage() {
+    this.currentImage = this.secondImage;
+  }
+
+  restoreImage() {
+    this.currentImage = this.firstImage;
+  }
 }
